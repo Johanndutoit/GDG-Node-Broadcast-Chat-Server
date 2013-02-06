98e9e775c9fb5f78f8c37090c8e7758648f1aa87
Johann du Toit <exxonno@gmail.com>
2013-02-06 12:29:32 +0200

    Basic TCP Chat Broadcast Server

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 // Require for Networking Functions
 var net = require('net');
 
+// All the connected clients
+var client_sockets = [];
+
 // Create Server with Callbacks
 var server = net.createServer(function (socket) {
+
+	// Add as connected client
+	client_sockets.push(socket);
 
 	// Welcome the User !
 	socket.write('Welcome to the GDG Chat Server!\r\n');
 
 	// Pipe response back to client
-	socket.pipe(socket);
+	socket.on('data', function(data){
+
+		for(var i = 0; i < client_sockets.length; i++) {
+
+			client_sockets[i].write(data);
+
+		}
+
+	});
 });
 
 // Listen on 1337 cause we are LEET
