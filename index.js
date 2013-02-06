068a2f6a48de261f87f2f66a13195e36931f1627
Johann du Toit <exxonno@gmail.com>
2013-02-06 12:37:45 +0200

    Basic Chat Server with username support.

--- index.js
+++ index.js
@@ -4,21 +4,44 @@ var net = require('net');
 // All the connected clients
 var client_sockets = [];
 
+// Count of Clients that have connected
+var count_of_clients_that_have_connected = 0;
+
 // Create Server with Callbacks
 var server = net.createServer(function (socket) {
 
 	// Add as connected client
 	client_sockets.push(socket);
 
+	// Update Count
+	count_of_clients_that_have_connected = count_of_clients_that_have_connected + 1;
+
 	// Welcome the User !
 	socket.write('Welcome to the GDG Chat Server!\r\n');
+
+	// Set a Default Name
+	socket.username = 'Guest #' + count_of_clients_that_have_connected
 
 	// Pipe response back to client
 	socket.on('data', function(data){
 
-		for(var i = 0; i < client_sockets.length; i++) {
+		// Quick cast to string
+		data = '' + data
 
-			client_sockets[i].write(data);
+		if(data.indexOf('/username') == 0) {
+
+			// Set the username of the user !
+			socket.username = data.split(' ')[1].replace('\r\n', '').replace('\n', '')
+
+		} else {
+
+			// Loop all the connected clients and send the message.
+			// This will include the current socket too !
+			for(var i = 0; i < client_sockets.length; i++) {
+
+				client_sockets[i].write(socket.username + ": " + data);
+
+			}
 
 		}
 
